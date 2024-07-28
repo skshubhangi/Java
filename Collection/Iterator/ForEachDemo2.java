6a3371a7644a7e005c9628274ab0598f8768c8ed
Shubhangi Kadam <shubhangikadam5553@gmail.com>
2024-07-29 00:26:41 +0530

    dailycode

---
+++ Collection/Iterator/ForEachDemo2.java
@@ -0,0 +1,19 @@
+package Java.Collection.Iterator;
+
+import java.util.*;
+
+public class ForEachDemo2 {
+    public static void main(String[] args){
+        ArrayList<Object> al = new ArrayList<Object>();
+        al.add(10);
+        al.add(20);
+        al.add("Shubhangi");
+        System.out.println(al);
+
+        for(Object x : al){
+            System.out.println(x);
+        }
+    }
+    
+}
+
